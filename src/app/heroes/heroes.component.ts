3b153cf8f25ec04513fc1dbfcd94fbcd36039eb3
misaki <pkccc3x@gmail.com>
2018-12-05 00:29:51 +0900

    リストの表示まで

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -11,14 +12,22 @@ export class HeroesComponent implements OnInit {
   //  hero = 'Windstorm';
   
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  // hero: Hero = {
+  //   id: 1,
+  //   name: 'Windstorm'
+  // };
+
+
+  heroes = HEROES;
+  selectedHero: Hero;
 
   constructor() { }
 
   ngOnInit() {
+  }
+   
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
