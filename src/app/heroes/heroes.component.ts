88ba27fe62a57686e02787218bbe0d77189449aa
misaki <pkccc3x@gmail.com>
2018-12-16 23:10:03 +0900

    サービスまで

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+// import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -18,16 +19,24 @@ export class HeroesComponent implements OnInit {
   // };
 
 
-  heroes = HEROES;
+  // heroes = HEROES;
   selectedHero: Hero;
+  heroes: Hero[];
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
    
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+
+  getHeroes(): void {
+    // this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 
 }
